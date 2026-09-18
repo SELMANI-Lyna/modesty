792eff526aeaf36e683fc84b2b395157f497d92f
Angel <lynaselmani16@gmail.com>
2026-09-18 20:13:22 +0100

    fix prisma client import

--- app/lib/prisma.js
+++ app/lib/prisma.js
@@ -1,6 +1,7 @@
 import { Pool } from "pg";
 import { PrismaPg } from "@prisma/adapter-pg";
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@/app/generated/prisma/client";
+
 
 const globalForPrisma = globalThis;
 
